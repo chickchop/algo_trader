1dfe0151e96a8aa180cc9756e688f6550d330e6b
Adam Ko <kohop8888@gmail.com>
2023-05-30 11:00:45 +0900

    [FIX] data_reader error

--- algo/data_reader.py
+++ algo/data_reader.py
@@ -176,7 +176,10 @@ def get_corp_last_book_data(category, text) :
             liability = int(corp_book.loc[corp_book['sj_div'].isin(['BS']) & corp_book['account_nm'].str.contains('부채총계')]["thstrm_amount"].iloc[0].replace(",","")) # 당기부채(부채총계)
             assets = equity + liability # 자산총계
             profit = int(corp_book[corp_book['sj_div'].isin(['IS', 'CIS']) & corp_book['account_nm'].str.contains('당기순이익')]["thstrm_amount"].iloc[0].replace(",","")) # 당기순이익
-            revenue = int(corp_book.loc[corp_book['sj_div'].isin(['IS']) & corp_book['account_nm'].str.contains('매출액')]["thstrm_amount"].iloc[0].replace(",","")) # 매출액
+            if corp_book['account_nm'].str.contains('매출액').sum() > 0 :
+                revenue = int(corp_book.loc[corp_book['sj_div'].isin(['IS', 'CIS']) & corp_book['account_nm'].str.contains('매출액')]["thstrm_amount"].iloc[0].replace(",","")) # 매출액
+            else :
+                revenue = int(corp_book.loc[corp_book['sj_div'].isin(['IS', 'CIS']) & corp_book['account_nm'].str.contains('영업수익')]["thstrm_amount"].iloc[0].replace(",","")) # 매출액 없을 시 영업 수익으로
             cash_flow = int(corp_book.loc[corp_book['sj_div'].isin(['CF']) & corp_book['account_nm'].str.contains('영업활동') & corp_book['account_nm'].str.contains('현금흐름')]["thstrm_amount"].iloc[0].replace(",","")) # 영업활동 현금흐름
             total_stock = int(corp_stock[corp_stock["se"] == "합계"]["distb_stock_co"].iloc[0].replace(",",""))
             stock_price = fdr.DataReader(symbol=corp_code, start=datetime(i,12,25), end=datetime(i,12,31))["Close"][-1]
@@ -221,4 +224,4 @@ def get_corp_last_book_data(category, text) :
     return corp_nm, corp_code, corp_finance_data, "Sucess"
 
 
-# get_corp_last_book_data("종목명", "셀트리온")
+# get_corp_last_book_data("종목명", "KX")
